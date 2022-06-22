5f707fb8401b1f5b53367b11dd90f0f994448c5a
mtplnr <tjsghd101@naver.com>
2022-06-22 13:35:18 +0900

    rt_driver

--- dsr_example/py/scripts/simple/roscontrol_test_traj.py
+++ dsr_example/py/scripts/simple/roscontrol_test_traj.py
@@ -14,7 +14,9 @@ from DSR_ROBOT import *
 __dsr__id = ROBOT_ID
 __dsr__model = ROBOT_MODEL
 
-control_type = "torque" #"gravity" #"velocity" # 
+
+### Sellect the controller options: position and velocity controller are not implemented yet (those make some purterbation during the execution)
+control_type = "torque" #"position" #"velocity" # "gravity" #
 
 traj = Trajectory(6)
 traj_flag = [0]*6
@@ -25,70 +27,32 @@ motion = 1
 rate=200
 # works with the default example only because I'm not dragging in their libraries to try and parameterize this.
 
-jscb_count=0
-rob_pos=[]
-
-def jointstatecallback(msg: JointState):
-    global jscb_count, rob_pos
-    jscb_count=jscb_count+1
-    rob_pos=list(msg.position)
 
 # ROS boilerplate
+
 print("Setting up node roscontrol_test, I will wiggle the second last joint ~10deg each way.")
 rospy.init_node('roscontrol_test')
 
-drt_servo_on=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/system/set_robot_control', SetRobotControl)
-retval=drt_servo_on(robot_control = 3)
-print(retval)
-
-drt_set_mode=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/system/set_robot_mode', SetRobotMode)
-retval=drt_set_mode(robot_mode = ROBOT_MODE_AUTONOMOUS)
-print(retval)
-
-drt_set_sys=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/system/set_robot_system', SetRobotSystem)
-retval=drt_set_sys(robot_system = ROBOT_SYSTEM_REAL)
-print(retval)
-
-
-drt_connect=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/realtime/connect_rt_control', ConnectRTControl)
-retval=drt_connect(ip_address =  "192.168.12.249", port = 12347)
-#retval=drt_connect(ip_address =  "192.168.0.114", port = 12347)
-if not retval:
-   raise SystemExit('realtime connect failed')
-print("connected")
-
-
-drt_setout=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/realtime/set_rt_control_output', SetRTControlOutput)
-retval=drt_setout(version = "v1.0", period = 0.001, loss = 10)
-
-if not retval:
-   raise SystemExit('realtime set output failed')
-print("settup")
-# start_rt_control!!
-drt_start=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/realtime/start_rt_control', StartRTControl)
-retval=drt_start()
-if not retval:
-   raise SystemExit('realtime start control failed')
-print("strated")
 
 drt_read=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/realtime/read_data_rt', ReadDataRT)
 drt_write=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/realtime/write_data_rt', WriteDataRT)
 
 print("setup has done")
 
-if control_type=="velocity":
+if control_type=="position":
+    command = rospy.Publisher("/dsr01m0609/servoj_rt_stream", ServoJRTStream, queue_size=1)
+elif control_type=="velocity":
     command = rospy.Publisher("/dsr01m0609/speedj_rt_stream", SpeedJRTStream, queue_size=1)
 elif control_type=="torque" or  control_type == "gravity":
     command = rospy.Publisher("/dsr01m0609/torque_rt_stream", TorqueRTStream, queue_size=1)
 governor=rospy.Rate(rate)
 
-drt_safety=rospy.ServiceProxy('/'+ROBOT_ID +ROBOT_MODEL+'/system/set_robot_safety_mode', SetRobotSafetyMode)
-print(SAFETY_MODE_AUTONOMOUS, SAFETY_MODE_EVENT_MOVE)
-retval=drt_safety(robot_safety_mode = SAFETY_MODE_AUTONOMOUS, robot_safety_mode_event=SAFETY_MODE_EVENT_MOVE)
-print(retval)
 
-if control_type=="velocity":
-    cmd_vel=SpeedJStream()
+if control_type=="position":
+    cmd_pos=ServoJRTStream()
+    cmd_pos.time=1/rate
+elif control_type=="velocity":
+    cmd_vel=SpeedJRTStream()
     cmd_vel.time=1/rate
 elif control_type == "torque" or control_type == "gravity" :
     cmd_tor=TorqueRTStream()
@@ -98,6 +62,7 @@ elif control_type == "torque" or control_type == "gravity" :
 init_time=time.time()
 qd=[0]*6
 qd_dot=[0]*6
+qd_ddot=[0]*6
 tor_ext_tmp=[0]*6
 buf_tor_ext=[0]*6
 tor_ext=[0]*6
@@ -118,8 +83,11 @@ while not rospy.is_shutdown():
     tor_ext_tmp = readdata.data.external_joint_torque
     
     if motion==1 and traj_flag[0]==0:
-        trj_q=[0,0,90,0,90,0]
+#        trj_q=[0,0,90,0,90,0]
+        trj_q=[0,45,90,0,-45,0]
+#        trj_q=[90,45,90,0,-45,0]
         motion+=1
+#        motion=0
         traj_flag=[1]*6
     elif motion==2 and traj_flag[0]==0:
         trj_q=[0,0,0,0,0,0]
@@ -143,7 +111,7 @@ while not rospy.is_shutdown():
         traj_flag=[1]*6     
     elif motion==7 and traj_flag[0]==0:
         trj_q=[20, 20, 40, 20, 0, 90]
-        motion=0
+        motion=1
         traj_flag=[1]*6    
         
         
@@ -157,21 +125,37 @@ while not rospy.is_shutdown():
             tmp_res,tmp_flag=traj.Polynomial5th(i,g_time)
             qd[i] = tmp_res[0]
             qd_dot[i] = tmp_res[1]
+            qd_ddot[i] = tmp_res[2]
             if tmp_flag == 0:
                 traj_flag[i]=0
     
-    
-    if control_type == "velocity":
+    if control_type == "position":
         # generate a signal for the robot to track
-        cmd_vel.vel[4]=10*sin(2*3.141592*time)
-        cmd_vel.acc[4]=10*2*3.141592*cos(2*3.141592*time)
+        for i in range(6):
+            cmd_pos.pos[i] = qd[i]
+            cmd_pos.vel[i] = qd_dot[i]
+            cmd_pos.acc[i] = qd_ddot[i]
+        
+        #publish command
+        command.publish(cmd_pos)
+    
+        print(cmd_pos.pos)
+        print(cmd_pos.vel)
+        print(cmd_pos.acc)
+        print("")
+        
+    elif control_type == "velocity":
+        # generate a signal for the robot to track
+        for i in range(6):
+            cmd_vel.vel[i] = qd_dot[i]
+            cmd_vel.acc[i] = qd_ddot[i]
         
         #publish command
         command.publish(cmd_vel)
     
-        #print(cmd_pos.vel)
-        #print(cmd_pos.acc)
-        #print("")
+        print(cmd_vel.vel)
+        print(cmd_vel.acc)
+        print("")
         
     elif control_type == "torque":
         
